a38c41b2ad4210ebb364536a95141ee25670d7c0
Sangyong Gwak <imyaman@netscape.net>
2020-05-07 09:00:56 +0000

    j1

--- j1/example.js
+++ j1/example.js
@@ -2,10 +2,10 @@
 
 const options = {
     hosts: {
-        domain: 'jitsi-meet.example.com',
+        domain: 'meet.devopskorea.org',
         muc: 'conference.jitsi-meet.example.com' // FIXME: use XEP-0030
     },
-    bosh: '//jitsi-meet.example.com/http-bind', // FIXME: use xep-0156 for that
+    bosh: '//meet.devopskorea.org/http-bind', // FIXME: use xep-0156 for that
 
     // The name of client node advertised in XEP-0115 'c' stanza
     clientNode: 'http://jitsi.org/jitsimeet'
